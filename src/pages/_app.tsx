19bf9cc8a31700ce9a25edebe41fedb78807b82d
Appo <archerkerr@icloud.com>
2024-12-06 08:06:49 +1100

    major fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import NavBar from '@/components/Site/Nav';
-import Footer from '@/components/Site/Footer';
 import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div style={{ backgroundColor: '#FBF7F5' }}> {/* Adding background color */}
       <Component {...pageProps} />
-    {/*} <Analytics /> */}
+      {/*} <Analytics /> */}
     </div>
   );
 }
