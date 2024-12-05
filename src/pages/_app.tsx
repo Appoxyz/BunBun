9caebbe832ec68412e9d6372aa7353362559b4fb
Appo <archerkerr@icloud.com>
2024-12-06 08:22:50 +1100

    Analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div style={{ backgroundColor: '#FBF7F5' }}> {/* Adding background color */}
       <Component {...pageProps} />
-      {/*} <Analytics /> */}
+      <Analytics />
     </div>
   );
 }
